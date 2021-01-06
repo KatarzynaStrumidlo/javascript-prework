f336f7f89e93e4746b7809d28ec4c20776da919f
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-01-06 13:42:37 +0100

    Add code to script.js

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+let computerMove = Math.floor (Math.random() * 3 + 1);
