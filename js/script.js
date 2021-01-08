4ad1a381b4a2c9855cbb1368259613efb167d78e
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-01-08 18:35:12 +0100

    Finish game and add style

--- js/script.js
+++ js/script.js
@@ -38,9 +38,9 @@ function playGame (playerInput){
   let gameResult = displayResult(argComputerMove, argPlayerMove);
   printMessage(gameResult);
 }
-document.getElementById('play-rock').addEventListener('click', playGame(1));
-document.getElementById('play-paper').addEventListener('click', playGame(2));
-document.getElementById('play-scissors').addEventListener('click', playGame(3));
+document.getElementById('play-rock').addEventListener('click', function(){playGame(1);});
+document.getElementById('play-paper').addEventListener('click', function(){playGame(2);});
+document.getElementById('play-scissors').addEventListener('click', function(){playGame(3);});
 
 
 
